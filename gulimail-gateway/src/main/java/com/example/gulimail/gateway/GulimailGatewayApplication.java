08f453da65af06d6b35caaf342f11ddbba46f5c0
kfocusone <kfocusone@outlook.com>
2025-07-28 16:06:45 +0800

    三方服务和网关

---
+++ gulimail-gateway/src/main/java/com/example/gulimail/gateway/GulimailGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.example.gulimail.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 1. 开启服务注册发现
+ *      配置 nacos 地址
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GulimailGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimailGatewayApplication.class, args);
+    }
+
+}
